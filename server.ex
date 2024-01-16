6a94898047e93f851fdbb58082269840db66cb92
Angelique B <angelique@apollosoftware.xyz>
2024-01-16 12:11:41 +0000

    work on server.ex

--- server.ex
+++ server.ex
@@ -1,8 +1,8 @@
 # Task 2
 # API where paxos will be connected to the application server
 defmodule ShopServer do
+
   def start(name, paxos_proc) do
-    # Start stuff
     pid = spawn(ShopServer, :init, [name, paxos_proc])
     pid = case :global.re_register_name(name, pid) do
       :yes -> pid
@@ -19,6 +19,7 @@ defmodule ShopServer do
       pax_pid: get_paxos_pid(paxos_proc),
       last_instance: 0,
       pending: {0, nil},
+      stock: 0,
     }
 
     run(state)
@@ -44,33 +45,85 @@ defmodule ShopServer do
     if msg, do: msg, else: wait_for_reply(r, attempt - 1)
   end
 
-  # functions for shop functionality
-  # i.e. add to cart, remove from cart, checkout, etc.
+  # Check if item is in stock
+  def check_stock(r) do
+    send(r, {:get_stock, self()})
+    receive do
+      {:stock, sto} -> sto
+      after 1000 -> :timeout
+    end
+  end
+
   # Add to cart
   def add_to_cart(r, item) do
-
+    if item < 0, do: raise("add_to_cart failed: item must be positive")
+    send(r, {:add_to_cart, self(), item})
+    case wait_for_reply(r, 5) do
+      {:add_to_cart_ok} -> :ok
+      {:add_to_cart_failed} -> :fail
+      {:abort} -> :fail
+      _ -> :timeout
+    end
   end
 
   # Remove from cart
   def remove_from_cart(r, item) do
-
-  end
-
-  # Checkout
-  def checkout(r) do
-
+    if item < 0, do: raise("remove_from_cart failed: item must be positive")
+    send(r, {:remove_from_cart, self(), item})
+    case wait_for_reply(r, 5) do
+      {:remove_from_cart_ok} -> :ok
+      {:remove_from_cart_failed} -> :fail
+      {:abort} -> :fail
+      _ -> :timeout
+    end
   end
 
   def run(state) do
     # Run stuff
+    state = receive do
+      {trans, client, _ } = t when trans = :add_to_cart or trans = :remove_from_cart ->
+        state = poll_for_decisions(state)
+        if Paxos.propose(state.pax_pid, state.last_instance + 1, t, 1000) == {:abort} do
+          send(client, {:abort})
+        else
+          %{state | pending: {state.last_instance + 1, client}}
+        end
 
+      {get_stock, client} ->
+        state = poll_for_decisions(state)
+        send(client, {:stock, state.stock})
+        state
+
+      {:poll_for_decisions} ->
+        poll_for_decisions(state)
+        _ -> state
+
+    end
+    run(state)
   end
 
   # Poll for decisions
-  def poll_for_decisions(r) do
-#    {add_to_cart, item} ->
-#    {remove_from_cart, item} ->
+  def poll_for_decisions(state) do
+    case Paxos.get_decision(state.pax_pid, i = state.last_instance) do
+       {add_to_cart, item} ->
+       state = case state.pending do
+         {i, client} ->
+           send(client, {:add_to_cart, item})
+           %{state | pending: {i+1, nil}}
+         _ -> state
+       end
+       poll_for_decisions(%{state | last_instance: i})
+
+       {remove_from_cart, item} ->
+        state = case state.pending do
+          {i, client} ->
+            send(client, {:remove_from_cart, item})
+            %{state | pending: {i+1, nil}}
+          _ -> state
+        end
+
+        state = %{state | stock: (if (sto = state.stock - item) < 0, do: 0, else: sto)}
+        poll_for_decisions(%{state | last_instance: i+1})
+    end
   end
-
-
 end
